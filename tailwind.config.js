0924d967f3ec848b001eced6dd28aaa68cff03d2
wesley williams <moto42@gmail.com>
2024-12-22 02:00:29 +0000

    add eleventy.config.js to Tailwind CSS content paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{njk,md}", "./src/**/*.svg",],
+  content: ["./src/**/*.{njk,md}", "./src/**/*.svg","./eleventy.config.js"],
   theme: {
     extend: {
       fontFamily: {
